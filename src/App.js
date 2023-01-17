251dc0c49da5876d082afede71757c0c5beeef86
Valentina Heredia <valu.heredia05@gmail.com>
2023-01-17 19:52:32 -0300

    inicio de item

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import MyButton from "./components/Button";
 import Examples from "./components/Examples";
+import {ButtonChild} from "./components/Button";
+import FlexWrapper from "./components/flexWrapper/FlexWrapper";
 
 function App() {
   
@@ -9,11 +11,13 @@ function App() {
   return (
     <>  
       <Examples />
-
-      <MyButton text="Botón A" />
-      <MyButton text="Otro botón" />
-      <MyButton text="soy el último"/> 
-    </>  
+      <FlexWrapper>
+      <MyButton text="Botón A" color="blue" />
+      <MyButton text="Otro botón" color="indigo" />
+      <MyButton text="soy el último" color="orange"/> 
+      <ButtonChild color="green">Ver detalle</ButtonChild>
+      </FlexWrapper>
+    </> 
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,19 @@
+import "./button/button.css";
 export default function MyButton (props) {
   console.log(props);
-    return <button>{props.text}</button>;
+    return (
+    <button style={ { backgroundColor: props.color } } className="btn">
+      {props.text}
+    </button>
+    );
+  }
+
+  export function ButtonChild(props){
+    console.log(props);
+
+    return (
+    <button style={ { backgroundColor: props.color } } className="btn">
+      {props.children}
+    </button>
+    );
   }
